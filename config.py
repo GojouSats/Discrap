6b1cee3f709145810355a047ab03b12950194268
L <60060180+GojouSats@users.noreply.github.com>
2022-07-17 15:06:49 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,19 @@
+#Im Lazy to make Proper Database :)
+
+
+API_ID   : int = 10101001
+API_HASH : str = 'str10101010'
+# Get your api hash and id here https://my.telegram.org/
+
+DISCORD_SELFBOT_TOKEN : str = 'discordaccounttoken' 
+# you can use real bot token too
+
+TELEGRAM_BOT_TOKEN :    str = '101010:telegrambottoken'
+# https://t.me/BotFather
+
+
+"""
+You can use telegram session string
+replace Bot.py token to session_string
+TelegramBot = Client("any_name_here",api_id=api_id,api_hash=api_hash,session_string=TELEGRAM_BOT_TOKEN,in_memory=True)
+"""
