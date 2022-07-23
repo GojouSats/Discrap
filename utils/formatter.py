e4fcbd163d28541e1677937f94b2c74e6522ec4a
L <60060180+GojouSats@users.noreply.github.com>
2022-07-24 00:25:57 +0800

    Update formatter.py

--- utils/formatter.py
+++ utils/formatter.py
@@ -1,20 +1,17 @@
 async def embed(message):
     """
-    EMBED FORMATTER:
+    EMBED FORMAT:
         - format an embed message
         - telegram can only embed 1 image
         - lazy to make photo sender :)
     """
     message = message.embeds[0]
-    if message.title:
-        title = message.title or ""
-
-    if message.description:
-        desc = message.description or ""
+    title = message.title or ""
+    desc = message.description or ""
 
     if message.image.url:
         image = message.image.url
-        format_message = f"{title}\n{desc} [ ]({image})"
+        format_message = f"{title}[ ͏]({image})\n{desc}"
     else:
         format_message = f"{title}\n{desc}"
 
@@ -24,11 +21,11 @@ async def embed(message):
 async def content(message):
     """
     CONTENT FORMAT:
-        - if message does have a file or image
-        - only fetch 1 image/file for now, because telegram does not auto folder image link
+        - if message doenst have image or no
+        - only fetch 1 image for now, because telegram does not auto folder image link
     """
     if message.attachments:
-        format_message = f"{message.content} [ ]({message.attachments[0]})"
+        format_message = f"{message.content}[ ͏]({message.attachments[0]})"
     else:
         format_message = f"{message.content}"
         
